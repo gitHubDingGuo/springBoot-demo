5ceb806610f0fafc53197214d763295c0ea5551d
liangdingguo <liangdingguo@qdama.cn>
2023-07-07 11:30:40 +0800

    first commit

---
+++ spring-boot-demo/spring-boot-demo-helloWorld/src/main/java/top/javahouse/demo/helloWorld/SpringBootDemoHelloWorldApplication.java
@@ -0,0 +1,23 @@
+package top.javahouse.demo.helloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDemoHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoHelloWorldApplication.class, args);
+    }
+
+    @GetMapping("/helloWorld")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("helloWorld", HttpStatus.OK);
+    }
+
+}
